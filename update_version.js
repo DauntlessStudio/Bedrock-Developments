1ba82c39cc7c819bee77ea5762efb83adc7fd5c3
DauntlessStudio <42518863+DauntlessStudio@users.noreply.github.com>
2024-03-13 11:53:46 -0500

    Removed Update Version.js

--- update_version.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let package = JSON.parse(fs.readFileSync('./package.json'));
-let version = package['version'];
-let index = String(fs.readFileSync('./ts/app/commands/base.ts'));
-index = index.replace(/const version = .+/, `const version = '${version}';`);
-fs.writeFileSync('./ts/app/commands/base.ts', index);
