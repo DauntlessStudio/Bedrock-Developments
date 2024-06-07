3d0d01ea96f273086e36520e5859f9368c94d123
DauntlessStudio <42518863+DauntlessStudio@users.noreply.github.com>
2024-06-07 16:39:42 -0500

    Updated Install Command

--- ts/app/commands/project/install.ts
+++ ts/app/commands/project/install.ts
@@ -1,7 +1,7 @@
 import { printVersion, } from "../base.js";
 import { program_project } from "./project.js";
 import { chalk } from "../../utils.js";
-import { MOJANG, MinecraftWorld } from "../../types/index.js";
+import { MOJANG } from "../../types/index.js";
 import * as fs from 'fs';
 import * as path from 'path';
 import { Directories, copySourceDirectory } from "../../file_manager.js";
@@ -23,18 +23,28 @@ function triggerProjectInstall() {
   if (fs.lstatSync(Directories.BEHAVIOR_PATH).isSymbolicLink()) {
     console.warn(`${chalk.yellow(`Ignoring BP as ${Directories.BEHAVIOR_PATH} is already a symlink`)}`);
   } else {
-    const current_bp = Directories.BEHAVIOR_PATH;
-    copySourceDirectory(current_bp, DEV_BP + "/" + path.basename(current_bp));
-    fs.rmSync(current_bp, {force: true, recursive: true});
-    fs.symlinkSync(DEV_BP + "/" + path.basename(current_bp), current_bp, "junction");
+    try {
+      const current_bp = Directories.BEHAVIOR_PATH;
+      copySourceDirectory(current_bp, DEV_BP + "/" + path.basename(current_bp));
+      fs.rmSync(current_bp, {force: true, recursive: true});
+      fs.symlinkSync(DEV_BP + "/" + path.basename(current_bp), current_bp, "junction");
+      console.log(`${chalk.green(`Created link ${Directories.BEHAVIOR_PATH}`)}`);
+    } catch (error) {
+      console.error(`${chalk.red(`Failed to link ${Directories.BEHAVIOR_PATH}, resources are busy. Try closing any applictions using these folders (i.e. vscode) and try command again`)}`);
+    }
   }
 
   if (fs.lstatSync(Directories.RESOURCE_PATH).isSymbolicLink()) {
-    console.warn(`${chalk.yellow(`Ignoring BP as ${Directories.RESOURCE_PATH} is already a symlink`)}`);
+    console.warn(`${chalk.yellow(`Ignoring RP as ${Directories.RESOURCE_PATH} is already a symlink`)}`);
   } else {
-    const current_rp = Directories.RESOURCE_PATH;
-    copySourceDirectory(current_rp, DEV_RP + "/" + path.basename(current_rp));
-    fs.rmSync(current_rp, {force: true, recursive: true});
-    fs.symlinkSync(DEV_RP + "/" + path.basename(current_rp), current_rp, "junction");
+    try {
+      const current_rp = Directories.RESOURCE_PATH;
+      copySourceDirectory(current_rp, DEV_RP + "/" + path.basename(current_rp));
+      fs.rmSync(current_rp, {force: true, recursive: true});
+      fs.symlinkSync(DEV_RP + "/" + path.basename(current_rp), current_rp, "junction");
+      console.log(`${chalk.green(`Created link ${Directories.RESOURCE_PATH}`)}`);
+    } catch (error) {
+      console.error(`${chalk.red(`Failed to link ${Directories.RESOURCE_PATH}, resources are busy. Try closing any applictions using these folders (i.e. vscode) and try command again`)}`);
+    }
   }
 }
