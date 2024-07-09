bed7a1324ab508343bfc3b203d696c13b558a90e
DauntlessStudio <42518863+DauntlessStudio@users.noreply.github.com>
2024-07-09 14:46:07 -0500

    Documentation Additions

--- bin/app/commands/command_map.d.ts
+++ bin/app/commands/command_map.d.ts
@@ -5,9 +5,29 @@ export interface CommandMapEntry<Args, Options> {
     commandOptions: (command: Command) => void;
     commandAction?: (args: Args, options: Options) => void;
 }
+/**
+ * @remarks A static class used to store information about commands, allowing for easy command copying.
+ */
 export declare class CommandMap {
     private static readonly entries;
+    /**
+     * @remarks Adds a command to the registry.
+     * @param name The name of the new command to add, note this is the internal registry name, not the display name.
+     * @param commandEntry The command data.
+     */
     static addCommand<Args = any, Options = OptionValues>(name: string, commandEntry: CommandMapEntry<Args, Options>): void;
+    /**
+     * @remarks Gets command data.
+     * @param name The name of the command to return.
+     * @returns The data associated with the command or undefined if no command with that name exists.
+     */
     static getCommandEntry(name: string): CommandMapEntry<any, OptionValues> | undefined;
+    /**
+     * @remarks Copies a command in the registery, allows for cross-subcommand aliases.
+     * @param name The internal name of the new command.
+     * @param source The internal name of the command to copy.
+     * @param commandName The display name of the new command.
+     * @param parent The parent the new command should be anchored to.
+     */
     static copyCommand(name: string, source: string, commandName: string, parent?: Command): Promise<void>;
 }
